0c0ead5599aa70ab454d7e34196d32067a4748f9
Rehan Siraj <rsiraj@r2hstudent.org>
2022-04-13 14:21:32 -0400

    mapped posts

--- src/main/js/pages/feed/feed.js
+++ src/main/js/pages/feed/feed.js
@@ -24,82 +24,19 @@ const Home = (props) => {
             <div id="home">
                 <div>
                     {/* Displays the posts stored in the posts state variable */}
-                    <Link
-                        to={`/story/${props.posts[0].id}`}
-                        key={props.posts[0].id}
-                    >
-                        <Post
-                            title={props.posts[0].title}
-                            postImage={props.posts[0].postImage}
-                            text={props.posts[0].text}
-                            author={props.posts[0].author}
-                            group={props.posts[0].group}
-                            date={props.posts[0].date}
-                            read_length={props.posts[0].read_length}
-                        />
+                    {props.posts.map((data) => (
+                        <Link to={`/story/${data.id}`} key={data.id}>
+                            <Post
+                                title={data.title}
+                                postImage={data.postImage}
+                                text={data.text}
+                                author={data.author}
+                                group={data.group}
+                                date={data.date}
+                                read_length={data.read_length}
+                            />
                         </Link>
-                        
-                        <Link
-                        to={`/story/${props.posts[1].id}`}
-                        key={props.posts[1].id}
-                    >
-                        <Post
-                            title={props.posts[1].title}
-                            postImage={props.posts[1].postImage}
-                            text={props.posts[1].text}
-                            author={props.posts[1].author}
-                            group={props.posts[1].group}
-                            date={props.posts[1].date}
-                            read_length={props.posts[1].read_length}
-                        />
-                        </Link>
-
-                        <Link
-                        to={`/story/${props.posts[2].id}`}
-                        key={props.posts[2].id}
-                    >
-                        <Post
-                            title={props.posts[2].title}
-                            postImage={props.posts[2].postImage}
-                            text={props.posts[2].text}
-                            author={props.posts[2].author}
-                            group={props.posts[2].group}
-                            date={props.posts[2].date}
-                            read_length={props.posts[2].read_length}
-                        />
-                        </Link>
-
-                        <Link
-                        to={`/story/${props.posts[3].id}`}
-                        key={props.posts[3].id}
-                    >
-                        <Post
-                            title={props.posts[3].title}
-                            postImage={props.posts[3].postImage}
-                            text={props.posts[3].text}
-                            author={props.posts[3].author}
-                            group={props.posts[3].group}
-                            date={props.posts[3].date}
-                            read_length={props.posts[3].read_length}
-                        />
-                        </Link>
-
-                        <Link
-                        to={`/story/${props.posts[4].id}`}
-                        key={props.posts[4].id}
-                    >
-                        <Post
-                            title={props.posts[4].title}
-                            postImage={props.posts[4].postImage}
-                            text={props.posts[4].text}
-                            author={props.posts[4].author}
-                            group={props.posts[4].group}
-                            date={props.posts[4].date}
-                            read_length={props.posts[4].read_length}
-                        />
-                        </Link>
-                    
-                    
+                    ))}
                 </div>
                 {/* Displays the New Writer UI Component */}
                 <NewWriter />
